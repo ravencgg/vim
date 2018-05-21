62f3a8e8fbfd9852c8d41925c1936fdb37b2aed6
ravencgg <raven.cgg@gmail.com>
2018-05-20 23:22:40 -0700

    Updating packages

--- vimfiles/my_files/run_build.bat
+++ vimfiles/my_files/run_build.bat
@@ -13,6 +13,7 @@ IF EXIST build.bat (
     REM If we aren't at the root directory, go up one and try again
     if "%cd:~3,1%"=="" (
         REM echo %cd% is root
+        echo Failed to find build.bat in any parent directory!
         goto :finished
     ) else (
         REM echo %cd% is not root
