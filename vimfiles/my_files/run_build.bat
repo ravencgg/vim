02ce1420c7ee4163eae27e5acf1b06ecf003c748
Chris Genova <raven.cgg@gmail.com>
2018-05-20 22:30:57 -0700

    Utility batch file to find and run a build.bat file

---
+++ vimfiles/my_files/run_build.bat
@@ -0,0 +1,27 @@
+@echo off
+
+pushd %cd%
+:retry
+
+SET currdir=%cd%
+
+IF EXIST build.bat (
+    call %currdir%\build.bat
+    goto :finished
+) ELSE (
+
+    REM If we aren't at the root directory, go up one and try again
+    if "%cd:~3,1%"=="" (
+        REM echo %cd% is root
+        goto :finished
+    ) else (
+        REM echo %cd% is not root
+        cd ..
+        goto :retry
+    )
+)
+
+:finished
+SET currdir=
+popd
+
